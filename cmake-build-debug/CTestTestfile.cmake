f5734169c260908abbe4efd26ebad4f7a8e88578
Boris <ermolovich.boris@yandex.ru>
2023-04-04 14:35:52 +0300

    создание cmake c gtest; создание cpp файлов с тестами без мок-объектов

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(unit_tests "/home/thematrix/TheMatrix2/workspace/reports/lab05/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;26;add_test;/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;0;")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;25;add_test;/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;0;")
