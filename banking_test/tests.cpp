5836edc8b97cf1b919c6a71cdf30ac11f01c171f
Boris <ermolovich.boris@yandex.ru>
2023-04-04 19:51:18 +0300

    добавление тестов с мок-объектами

--- banking_test/tests.cpp
+++ banking_test/tests.cpp
@@ -1,32 +1,72 @@
 #include "Transaction.h"
 #include "Account.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-TEST(Account, Balance) {
-    Account account(1234567, 100);
-    EXPECT_EQ(account.GetBalance(), 100);
-    account.Lock();
-    account.ChangeBalance(50);
-    account.Unlock();
-    EXPECT_EQ(account.GetBalance(), 150);
+class Account_mock : public Account {
+public:
+    Account_mock(int id, int balance) : Account(id, balance) {}
+
+    MOCK_CONST_METHOD0(GetBalance, int());
+    MOCK_METHOD1(ChangeBalance, void(int diff));
+    MOCK_METHOD0(Lock, void());
+    MOCK_METHOD0(Unlock, void());
+};
+
+class Transaction_mock : public Transaction {
+public:
+    MOCK_METHOD3(Make, bool(Account & from, Account & to, int sum));
+    MOCK_METHOD1(Debit, void(int diff));
+    MOCK_METHOD1(Credit, void(int diff));
+};
+
+TEST(Account, Mock) {
+    Account_mock acc(1, 100);
+    EXPECT_CALL(acc, GetBalance()).Times(1);
+    EXPECT_CALL(acc, ChangeBalance(testing::_)).Times(2);
+    EXPECT_CALL(acc, Lock()).Times(2);
+    EXPECT_CALL(acc, Unlock()).Times(1);
+    acc.GetBalance();
+    acc.ChangeBalance(100); // throw
+    acc.Lock();
+    acc.ChangeBalance(100);
+    acc.Lock(); // throw
+    acc.Unlock();
 }
 
-TEST(Account, Locking) {
-    Account account(7654321, 200);
-    EXPECT_THROW(account.ChangeBalance(50), std::runtime_error);
-    account.Lock();
-    EXPECT_THROW(account.Lock(), std::runtime_error);
-    EXPECT_NO_THROW(account.ChangeBalance(49));
-    EXPECT_EQ(account.GetBalance(), 249);
+TEST(Account, Test) {
+    Account acc(1, 100);
+    EXPECT_EQ(acc.id(), 1);
+    EXPECT_EQ(acc.GetBalance(), 100);
+    EXPECT_NO_THROW(acc.Lock());
+    acc.ChangeBalance(100);
+    EXPECT_EQ(acc.GetBalance(), 200);
 }
 
-TEST(Transaction, DataBase) {
-    Account from(123, 1000);
-    Account to(789, 500);
-    Transaction transaction;
-    transaction.set_fee(100);
-    EXPECT_TRUE(transaction.Make(from, to, 500));
-    EXPECT_FALSE(transaction.Make(from, to, 100));
-    EXPECT_EQ(from.GetBalance(), 1000);
-    EXPECT_EQ(to.GetBalance(), 400);
+TEST(Transaction, Mock) {
+    Transaction_mock tr;
+    Account acc1(1, 50);
+    Account acc2(2, 500);
+    EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_)).Times(6);
+    tr.set_fee(100);
+    tr.Make(acc1, acc2, 199);
+    tr.Make(acc2, acc1, 500);
+    tr.Make(acc2, acc1, 300);
+    tr.Make(acc1, acc1, 0);
+    tr.Make(acc1, acc2, -1);
+    tr.Make(acc1, acc2, 99);
+}
+
+TEST(Transaction, Test) {
+    Transaction tr;
+    Account ac1(1, 50);
+    Account ac2(2, 500);
+    tr.set_fee(100);
+    EXPECT_EQ(tr.fee(), 100);
+    EXPECT_THROW(tr.Make(ac1, ac1, 0), std::logic_error);
+    EXPECT_THROW(tr.Make(ac1, ac2, -1), std::invalid_argument);
+    EXPECT_THROW(tr.Make(ac1, ac2, 99), std::logic_error);
+    EXPECT_FALSE(tr.Make(ac1, ac2, 199));
+    EXPECT_FALSE(tr.Make(ac2, ac1, 500));
+    EXPECT_TRUE(tr.Make(ac2, ac1, 300));
 }
