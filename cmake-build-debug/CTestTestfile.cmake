4c3cac6ca2326c72ce0294561121b14256652b7f
Boris <ermolovich.boris@yandex.ru>
2023-04-12 18:29:42 +0300

    updating

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/thematrix/TheMatrix2/workspace/reports/lab05/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;25;add_test;/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;0;")
+add_test(test "/home/thematrix/TheMatrix2/workspace/reports/lab05/cmake-build-debug/tests")
+set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;25;add_test;/home/thematrix/TheMatrix2/workspace/reports/lab05/CMakeLists.txt;0;")
